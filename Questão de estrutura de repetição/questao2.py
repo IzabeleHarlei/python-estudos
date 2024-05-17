b2145fe74a49b13e46850f3e20ec5b0ab7e68c59
JemmEdu16 <159553167+IzabeleHarlei@users.noreply.github.com>
2024-05-17 11:04:14 -0300

    eccr
    2

--- Questão de estrutura de repetição/questao2.py
+++ Questão de estrutura de repetição/questao2.py
@@ -1,5 +1,4 @@
 palavra = input()
-for vogal in palavra:
- if vogal == "a" or vogal == "o" or vogal == "e" or vogal == "i" or vogal == "u":
-  contagens_vogal = palavra.count(vogal)
-  print(contagens_vogal)
+vogal = "aeiouAEIOU"
+contagens_vogal = palavra.count(vogal)
+print(contagens_vogal)
